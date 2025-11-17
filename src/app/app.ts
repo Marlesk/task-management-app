d8140c1b27f0d7028f7e8dabc4e159fc2ca280a3
Mar. Leska <mariales.leska@gmail.com>
2025-11-17 21:00:14 +0200

    feat(app): add initial AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Header } from './components/header/header';
+import { Main } from './components/main/main';
+import { Task } from './shared/interfaces/task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Main ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('task-management');
+  searchTerm: string = '';    // Η τιμή που θα πάρει από το child Header μέσω του Output
+
 }
